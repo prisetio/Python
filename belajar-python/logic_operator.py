703a4419d9623047171dea493e64b17fbedec1f5
Priyo Setio <priyos.17@gmail.com>
2020-09-06 18:50:20 +0700

    Add files via upload

---
+++ belajar-python/logic_operator.py
@@ -0,0 +1,13 @@
+#Operator Logika
+
+print("Operator 'And'")
+print(True and True)
+print(True and False)
+print(False and True)
+print(False and False)
+
+print("Operator 'Or'")
+print(True or True)
+print(True or False)
+print(False or True)
+print(False or False)
