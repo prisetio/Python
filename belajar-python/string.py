4b669d37e44278f409fb8f3eda06e640f7c48a10
Priyo Setio <priyos.17@gmail.com>
2020-09-06 14:33:20 +0700

    Add files via upload

---
+++ belajar-python/string.py
@@ -0,0 +1,11 @@
+#Belajar String Pemula
+
+nama = "Priyo Setio"
+kota =  'Depok'
+alamat = 'Jalan Bla.. Bla..'
+
+nama_depan = "Priyo"
+nama_belakang = "Setio"
+nama_lengkap = nama_depan + " " + nama_belakang
+
+print(nama_lengkap)
