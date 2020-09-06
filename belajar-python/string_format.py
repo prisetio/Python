21a2e82cea39e9eb8977a1b91292bab08921e7cc
Priyo Setio <priyos.17@gmail.com>
2020-09-06 14:39:18 +0700

    Add files via upload

---
+++ belajar-python/string_format.py
@@ -0,0 +1,8 @@
+nama_depan = "Priyo"
+nama_tengah = "Setio"
+nama_belakang = "Bangun"
+
+sapa = f"Halo {nama_depan} {nama_tengah} {nama_belakang}"
+#untuk string format harus menambahkan F di bagian depan string
+
+print(sapa)
