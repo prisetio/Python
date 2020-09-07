12e7f319f3ade945cab0fca82204518e20cd2fbf
Priyo Setio <priyos.17@gmail.com>
2020-09-08 06:41:10 +0700

    Add files via upload

---
+++ belajar-python/elif.py
@@ -0,0 +1,15 @@
+# Belajar Elif => Else If
+
+menu_pilihan = input("Silahkan Anda pilih menu 1-3 : ")
+
+if menu_pilihan == "1":
+    print("Anda memilih menu 1")
+
+elif menu_pilihan == "2":
+    print("Anda memilih menu 2")
+
+elif menu_pilihan == "3":
+    print("Anda memilih menu 3")
+
+else:
+    print("Menu tidak tersedia")
