3dbab40ab8c3d2d833c6e693e5914983d78d8602
Priyo Setio <priyos.17@gmail.com>
2020-09-08 06:07:03 +0700

    Add files via upload

---
+++ belajar-python/operator_perbandingan.py
@@ -0,0 +1,16 @@
+# Operator Perbandingan
+
+# > Lebih dari
+# < Kurang dari
+# >= Lebih dari sama dengan
+# <= Kurang dari sama dengan
+# == Sama dengan
+# != Tidak sama dengan
+
+print(5 <= 3)
+print(10 == 10)
+print(7 != 9)
+
+# Keunikan dari sama dengan (==) dan tidak sama dengan (!=)
+print("priyo" == "priyo")
+print("priyo" != "priyo")
