bf1131408315d40922813a1b3100096fcf76dc41
Priyo Setio <priyos.17@gmail.com>
2020-09-08 06:16:48 +0700

    Add files via upload

---
+++ belajar-python/if.py
@@ -0,0 +1,18 @@
+# Belajar If-Statement
+
+#if kondisi:
+#    melakukan apa?
+#    melakukan hal lain
+#dan seterusnya
+
+orang = 100
+zombie = 100
+
+if orang < zombie:
+    print("Kabur, dunia mau kiamat")
+
+if orang > zombie:
+    print("Mari basmi zombie")
+
+if orang == zombie:
+    print("Mari kita berperang")
