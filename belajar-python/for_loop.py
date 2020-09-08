6d3faa89c7f5d09361bb8606199de6f485305adb
Priyo Setio <priyos.17@gmail.com>
2020-09-08 13:08:18 +0700

    Add files via upload

---
+++ belajar-python/for_loop.py
@@ -0,0 +1,10 @@
+# Belajar For Loop
+
+pelanggan = ["priyo", "setio"]
+
+pelanggan.append("Bening")
+pelanggan.append("Saraswati")
+
+# Mengakses semua nama pelanggan?
+for nama in pelanggan:
+    print(f"Nama pelanggan : {nama}")
