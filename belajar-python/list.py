0add07bc1cbb11c0ac6bc6f7bd4c7cc2743d0809
Priyo Setio <priyos.17@gmail.com>
2020-09-08 10:00:58 +0700

    Add files via upload

---
+++ belajar-python/list.py
@@ -0,0 +1,14 @@
+# Belajar List
+
+nama = ["Priyo", "Yunus", "Adi"]
+nama.append("Deden")
+
+print(nama[0])
+print(nama[1])
+print(nama[2])
+print(nama[3])
+
+print(len(nama))
+
+nama.remove("Yunus")
+print(nama)
