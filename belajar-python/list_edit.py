43de5affe9e7304245d8289a2057fbc189d530a2
Priyo Setio <priyos.17@gmail.com>
2020-09-08 12:14:51 +0700

    Add files via upload

---
+++ belajar-python/list_edit.py
@@ -0,0 +1,16 @@
+# Membuat List
+nama = ["priyo", "setio"]
+
+# Menambah Data ke List
+nama.append("bangun")
+
+# Mengakses List
+nama[0]
+
+# Menghapus Data dari List
+nama.remove("priyo")
+
+# Mengubah Data di List
+print(nama)
+nama[1] = "bening"
+print(nama)
