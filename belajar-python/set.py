ef7d62624cbb44f7ec32bd430a1530b3489efe60
Priyo Setio <priyos.17@gmail.com>
2020-09-10 10:29:49 +0700

    Add files via upload

---
+++ belajar-python/set.py
@@ -0,0 +1,16 @@
+# Belajar Set
+
+# list => []
+# tuple => ()
+# set => {}
+
+nama = {"Priyo", "Bening", "Priyo"}
+
+nama.add("Saraswati")
+
+for data in nama:
+    print(data)
+
+nama.remove("Saraswati")
+
+print(nama)
