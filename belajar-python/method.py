c7642aefedd1e1ae6c289128bdf831d56595a954
Priyo Setio <priyos.17@gmail.com>
2020-09-10 10:31:09 +0700

    Add files via upload

---
+++ belajar-python/method.py
@@ -0,0 +1,17 @@
+# Belajar membuat Method / Function
+
+nama = []
+
+def print_nama():
+    print("==================")
+    for data in nama:
+        print(data)
+
+nama.append("priyo")
+print_nama()
+
+nama.append("bening")
+print_nama()
+
+nama.append("setio")
+print_nama()
